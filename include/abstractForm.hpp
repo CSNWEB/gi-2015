7f6138a9e6671e55c1e0bd13f0720e20ed9a1c51
Max Katzmann <m.katzmann@me.com>
2015-11-05 21:13:00 +0100

    Added comments to the header file of AbstractForm.

--- include/abstractForm.hpp
+++ include/abstractForm.hpp
@@ -1,3 +1,10 @@
+/**
+ *  Class AbstractForm
+ *
+ *  An AbstractForm describes a form of any shape which is defined
+ *  by its "points". An AbstractForm has a name and one can get the size of its
+ *  bounding box and its area.
+ */
 #ifndef ABSTRACTFORM_H
 #define ABSTRACTFORM_H
 
@@ -14,28 +21,101 @@ using namespace std;
 class AbstractForm
 {
 private:
+    
+    /**
+     *  The number of points that the form is made of.
+     */
 	int number_of_points;
+    
+    /**
+     *  The name of the form.
+     */
 	string name;
 
+    /**
+     *  The points that make up the form.
+     */
 	vector<Point> points;
 	
-	float dx,dy; // size of bounding box
+    /**
+     *  The size of the bounding box of the form in x and y direction.
+     */
+	float dx,dy;
+    
+    /**
+     *  The size of the are of the form.
+     */
 	float size_of_area;
 	
 public:
+    
+    /**
+     *  Default constructor
+     */
 	AbstractForm(){};
+    
+    /**
+     *  Constructor that initializes the form with a name and its points
+     *
+     *  @param name     A string that represents the name of the form.
+     *  @param points   A vector of type "Point" that form the form.
+     */
 	AbstractForm(string name, vector<Point> points);
 
-	// given a Plane and a starting Position, add this form to the plane:
+    /**
+     *  Adds the form to a plane at the given position.
+     *
+     *  @param s     The plane that the form is to be added to.
+     *  @param pos_x The position on the x-axis where the form will be added.
+     *  @param pos_y The position on the y-axis where the form will be added.
+     */
 	void create_form_in_plane(Plane *s, float pos_x, float pos_y);
 
+    /**
+     *  A Debugging function that prints the properties of the form to the
+     *  console
+     */
 	void _d_print_abstract_form();
 
+    /**
+     *  Get the number of points that this form is made of.
+     *
+     *  @return The number of points that the form is made of.
+     */
 	int get_number_of_points(){return number_of_points;};
+    
+    /**
+     *  Get the position of the form on the x-axis.
+     *
+     *  @return A float representing he position of the form on the x-axis.
+     */
 	float get_dx(){return dx;};
+    
+    /**
+     *  Get the position of the form on the y-axis.
+     *
+     *  @return A float representing he position of the form on the y-axis.
+     */
 	float get_dy(){return dy;};
-	Point get_point_by_number(int num){return points[num];};
+    
+    /**
+     *  Get the certain point of the ones that make up the form.
+     *
+     *  @param index The index of the point in the array containing the points of the form.
+     *
+     *  @return The "Point" at the specified index.
+     */
+	Point get_point_at_index(int index){return points[index];};
 
+    /**
+     *  Creates a ConvexShape representation of this AbstractForm.
+     *
+     *  @param pos_x The position of the form on the x-axis.
+     *  @param pos_y The position of the form on the y-axis.
+     *  @param scale The scale of the form.
+     *
+     *  @return An object of type "ConvexShape" that represents this AbstractForm.
+     */
 	sf::ConvexShape get_shape(int pos_x, int pos_y, int scale);
 };
 
