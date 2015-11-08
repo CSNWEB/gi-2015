e27f371405cc41479d655effc3eb0ba40d4878f7
Max Katzmann <m.katzmann@me.com>
2015-11-08 20:51:57 +0100

    Proceeded adding comments.
    Added comments to the header file of Edge and updated the comments of
    the header file of Form.

--- include/edge.hpp
+++ include/edge.hpp
@@ -1,3 +1,11 @@
+/**
+ *  Class Edge
+ *
+ *  An Edge consists of two points. It has a length and one can get the distance
+ *  it has on the x- and y-axis.
+ *  It can be moved and rotated and one can check whether an edge crosses
+ *  another edge.
+ */
 #ifndef EDGE_H
 #define EDGE_H
 
@@ -8,19 +16,63 @@
 class Edge
 {
 private:
+    
+    /**
+     *  The points that define the edge.
+     */
 	Point *p1, *p2;
 	
 public:
+    
+    /**
+     *  Default constructor
+     */
 	Edge(){};
 
+    /**
+     *  Constructor to initialize an edge with its two points.
+     */
 	Edge(Point *p1, Point *p2);
 	
+    /**
+     *  Move the edge relative to its current position by the amounts given in
+     *  x- and y-direction.
+     *
+     *  @param dx A float defining how much the edge is moved on the x-axis.
+     *  @param dy A float defining how much the edge is moved on the y-axis.
+     */
 	void move_rel(float dx, float dy);
-	void rotate(float center_x, float center_y, float degree);
+    
+    /**
+     *  Rotates the edge around the passed center by the given degree.
+     *
+     *  @param center_x A float defining the position of the center on the x-axis.
+     *  @param center_y A float defining the position of the center on the y-axis.
+     *  @param degree   A float describing the amount by which the edge is rotated.
+     */
+	void rotate(float center_x, float center_y, float degree); // TODO: Maybe use Point for center?
 
+    /**
+     *  Check whether the receiver crosses the passed edge.
+     *
+     *  @param other The Edge that the cross-check is done against.
+     *
+     *  @return true if the receiver and other cross, false if not.
+     */
 	bool check_if_crosses(Edge *other);
 	
+    /**
+     *  The length of the edge
+     *
+     *  @return A float resembling the length of the edge.
+     */
 	float length(){return p1->get_distance_to(p2);};
+    
+    /**
+     *  The distance the edge covers on the x- and y-axis respectively.
+     *
+     *  @return A float representing the distance the edge covers on the x- and y-axis respectively.
+     */
 	float get_edge_x(){return p2->get_x()-p1->get_x();};
 	float get_edge_y(){return p2->get_y()-p1->get_y();};
 };
--- include/form.hpp
+++ include/form.hpp
@@ -1,3 +1,11 @@
+/**
+ *  Class Form
+ *
+ *  A Form is the representation of an AbstractForm.
+ *  Its 'mother' is the form that this one belongs to. It's made up of points
+ *  and edges and has a bounding box. It can be moved, rotated and one can check
+ *  whether it crosses another Form.
+ */
 #ifndef FORM_H
 #define FORM_H
 
@@ -15,76 +23,99 @@ using namespace std;
 class Form
 {
 private:
-	/**
-	 * The abstract form this form is derived of
-	 */
-		AbstractForm *mother;
+    
+    /**
+     *  The abstract form this form is derived of
+     */
+    AbstractForm *mother;
 	
 	/**
-	 * The number of outer edges this form has.
-	 * Note that this is equal to the number of points that define this form
+	 *  The number of outer edges this form has.
+	 *  Note that this is equal to the number of points that define this form
 	 */
-		int number_of_edges;
+    int number_of_edges;
 
-		vector<Edge> edges;
-		vector<Point> points;
+    /**
+     *  A vector of edges that make up this Form.
+     */
+    vector<Edge> edges;
+    
+    /**
+     *  A vector of points that make up this Form.
+     */
+    vector<Point> points;
 	
 	/**
-	 * parameters definind the bounding box of this form:
+	 *  Parameters definind the bounding box of this form:
 	 */
-		float x_min;
-		float x_max;
-		float y_min;
-		float y_max;
+    float x_min, x_max, y_min, y_max;
 	
 public:
 	/**
-	 * Default constructor
+	 *  Default constructor
 	 */
-		Form();
+    Form();
 
 	/**
-	 * Constructor that creates a form specified by an AbstractForm, bit without a specific position
+	 *  Constructor that creates a form specified by an AbstractForm, bit without
+     *  a specific position
 	 */
-		Form(AbstractForm *mother);
+    Form(AbstractForm *mother);
 
 	/**
-	 * Constructor that creates a Form at given coordinates (top left corner of bounding box)
+	 *  Constructor that creates a Form at given coordinates (top left corner of
+     *  bounding box)
 	 */
-		Form(AbstractForm *mother, float pos_x, float pos_y);
+    Form(AbstractForm *mother, float pos_x, float pos_y); // TODO: Use Point?
 	
-	/**
-	 * Getter functions for bounding box
-	 */
+    /**
+     *  The coordinates of the bounding box. Described by the minimum and
+     *  maximum values in x- and y-direction that contain the forms points.
+     *
+     *  @return A float representing the position of the bounding box at minimum and maximum position on the x- and y-axis respectively.
+     */
 	float get_bounding_xmin(){return x_min;};
 	float get_bounding_xmax(){return x_max;};
 	float get_bounding_ymin(){return y_min;};
 	float get_bounding_ymax(){return y_max;};
 	
-	/**
-	 * Function to move this form relatively
-	 */
+    /**
+     *  Moves the Form relative to its current position by the passed amount in
+     *  x- and y-direction.
+     *
+     *  @param dx A float representing the amount that the Form is moved by on the x-axis.
+     *  @param dy A float representing the amount that the Form is moved by on the y-axis.
+     */
 	void move_rel(float dx, float dy);
 
-	/**
-	 * Function to rotate this form around a center by a specified degree
-	 */
+    /**
+     *  Rotates the form around the passed center by the amount given by degree.
+     *
+     *  @param center_x The x-coordinate of the center point that the Form is rotated around.
+     *  @param center_y The y-coordinate of the center point that the Form is rotated around.
+     *  @param degree   The amount that the Form is rotated by.
+     */
 	void rotate(float center_x, float center_y, float degree);
 	
-	/**
-	 * Function checks if this form overlaps with another form
-	 *
-	 * @param other		An other form for which overlapping is checked
-	 */
+    /**
+     *  Checks if the receiver overlaps with the passed Form.
+     *
+     *  @param other The form that the overlapping is checked against.
+     *
+     *  @return true, if the receiver and the Form other overlap, false if not.
+     */
 	bool check_for_overlap(Form *other);
 	
-	/**
-	 * overloaded version of above function,
-	 * that saves minimum distance for each two edges in float-matrix; or zero of edges cross.
-	 *
-	 * @param other			An other form for which overlapping is checked
-	 * @param crossings		a 2-dim float matrix that stores minimum distance for each pair of points
-	 */
+    /**
+     *  Checks if the receiver overlaps with the passed Form.
+     *  Additionally saves the minimum distance for every two edges in a
+     *  float-matrix or zero if the edges cross.
+     *
+     *  @param other     The form that the overlapping is checked against.
+     *  @param crossings A two-dimensional float-matrix describing the minimum distance between two points of the form.
+     *
+     *  @return True if the receiver and the other Form overlap, false if not.
+     */
 	bool check_for_overlap(Form *other, floatmat* crossings);
 };
 
