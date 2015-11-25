eb636f9f13f863d0b0eecc6d89feb256e8cb7f52
Max Katzmann <m.katzmann@me.com>
2015-11-25 15:35:38 +0100

    Started implementing Validator
    - started implementation of Validator, which checks whether a Setting
    or Plane is valid
    - Parts of this implementation were added in an earlier commit
    accidentally

---
+++ include/validator.hpp
@@ -0,0 +1,55 @@
+/**
+ *  Class Validator
+ *
+ *  Validator check whether a given Setting or plane is valid.
+ *
+ *  For a Setting it is checked, wheter the required number of each form is
+ *  placed, as well as whether forms exceed the edges of a plane or whether
+ *  forms overlap each other.
+ *  These are also criteria for which a validity of a plane is checked.
+ */
+#ifndef VALIDATOR_H
+#define VALIDATOR_H
+
+#include <iostream>
+#include <vector>
+
+#include "setting.hpp"
+#include "plane.hpp"
+
+class Validator
+{
+private:
+    
+public:
+    
+    /**
+     *  Checks whether a plane is valid.
+     *
+     *  Criteria are:
+     *  - the forms on the plane do not overlap
+     *  - the forms on the plane do not exceed the bounds of the plane
+     *
+     *  @param plane The plane whose validity is to be checked.
+     *
+     *  @return true if the plane is valid, false if not.
+     */
+    static bool validate_plane(Plane *plane);
+    
+    /**
+     * Checks whether a setting is valid.
+     *
+     * Criteria are:
+     * - the required number of each form has been placed on the planes
+     * - the forms do not exceed the bounds of the planes
+     * - the forms do not overlap
+     *
+     * @param setting   The Setting which is checked to be valid.
+     *
+     * @return true, if the setting is valid. false, if not.
+     */
+    static bool validate_setting(Setting *setting);
+    
+};
+
+#endif
