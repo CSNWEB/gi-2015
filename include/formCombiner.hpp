4ed4317b9a118bc9d9c2b8e3872cd9becac4bfb4
Andreas <angohz@ymail.com>
2016-01-10 16:47:28 +0100

    Clean header of FormCombiner

--- include/formCombiner.hpp
+++ include/formCombiner.hpp
@@ -25,27 +25,19 @@ class FormCombiner
 {
 private:
 	/*!
-	 *
+	 *  Two AbstractFormConfigurations for which an optimized Tuple should be computed.
 	 */
 	AbstractFormConfiguration *form_config_1;
 
+	/*!
+	 *  Two AbstractFormConfigurations for which an optimized Tuple should be computed.
+	 */
 	AbstractFormConfiguration *form_config_2;
 
 	/*!
 	 *  A pointer to the problem. Needed to decide if a combined form still fits on plane.
 	 */
 	Problem* problem;
-
-	/*!
-	 *  A pointer to the first AbstractForm
-	 */
-	//AbstractForm* abstract_form_1;
-
-	/*!
-	 *  A list of possible AbstractFormConfigurations, sorted by optimality of area utilization of the bounding box
-	 */
-	//list<AbstractFormConfigurationTuple> possible_configurations;
-
 public:
 	/*!
 	 *  Default Constructor
@@ -53,22 +45,11 @@ public:
 	FormCombiner(Problem *p, AbstractFormConfiguration *form_config_1, AbstractFormConfiguration *form_config_2);
 
 	/*!
-	 *  Search all other forms for optimal configurated tuple
-	 */
-	//void search_for_optimal_configuratioon_global();
-
-	/*!
 	 *  Compute optimal configuration for the two AbstractForms and creates an AbstractFormConfigurationTuple
 	 *
 	 *  @return 	the constructed AbstractFormConfigurationTuple, or Null if no efficient combination was found
 	 */
 	AbstractFormConfigurationTuple compute_optimal_configuration();
-
-	/*!
-	 *  Get the best AbstractFormConfiguration that does not use any AbstractForm from a given vector
-	 */
-	//AbstractFormConfigurationTuple get_best_tuple(vector<int> forbidden_forms);
-
 };
 
 #endif
